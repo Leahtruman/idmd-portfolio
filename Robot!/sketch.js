a6e86dc10496d5e9c52de4cf403be94214dfbd36
Leahtruman <60490234+Leahtruman@users.noreply.github.com>
2020-02-05 10:07:01 -0800

    Robot Attempt 1
    :)

---
+++ Robot!/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  rect()
+  fill(155, 196, 170)
+  background(220);
+  circle(200,170,130)
+  fill(255, 242, 148)
+  ellipse(170,165,50,80)
+  ellipse(230,165,50,80)
+  fill(0)
+  ellipse(230,165,20,50)
+  fill(0)
+  ellipse(170,165,20,50)
+  fill(225)
+  ellipse(160,150,15,15)
+  fill(225)
+  ellipse(220,150,15,15)
+  rect(145,45,5,90)
+  rect(245,45,5,90)
+  fill(252, 73, 3)
+  ellipse(247,45,20,20)
+  ellipse(147,45,20,20)
+  
+  
+  
+  
+}

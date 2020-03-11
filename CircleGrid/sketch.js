bd22cb5d686fd926edf629790f256e095c6d33e6
Leahtruman <60490234+Leahtruman@users.noreply.github.com>
2020-03-11 13:33:32 -0700

    played around with canvas size

--- CircleGrid/sketch.js
+++ CircleGrid/sketch.js
@@ -1,9 +1,11 @@
+//here, the pretty much all the code is in setup bacause we don't need to run the draw function 60 times a second
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 800);
   background(200);
+  //variables for the diameter and radius are set up to be used later
   var dia = width / 10;
   var rad = dia / 2;
-
+  //using a for loop, the identical ellipses drawn in a grid, running the same loop of code until the canvas is exactly filled up
   for (i = rad; i <= width; i = i + dia) {
     for (j = rad; j <= height; j = j + dia) {
       ellipse(i, j, dia, dia);
@@ -13,4 +15,5 @@ function setup() {
 
 function draw() {
   //background(220);
+  //nothing needs to go here
 }
